7f71a9925d943d2df1c99aec1fcf3d688a589e21
SumitKumarDas1102 <sumit.das.1102@gmail.com>
2020-11-22 21:00:09 +0530

    push latest code

--- automationForWeInvest/src/main/java/automationForWeInvestPages/InvestmentNowPage.java
+++ automationForWeInvest/src/main/java/automationForWeInvestPages/InvestmentNowPage.java
@@ -1,8 +1,18 @@
 package automationForWeInvestPages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import weInvestBase.ProjectBase;
 
 public class InvestmentNowPage extends ProjectBase{
+	
+	@FindBy(xpath="//div//h3[@class = 'header-text']")
+	private WebElement headerText;
+	
+	@FindBy(xpath="//div//span[contains(@class, 'header-text') and contains(., 'What does your portfolio contain?')]")
+	private WebElement yourPorfolioContain;
+	
 	
 
 }
